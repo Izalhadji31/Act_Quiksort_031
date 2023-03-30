0286b9fd77d6a32c4e60cef70bdb99da2f4a57f0
Izalhadji31 <ryzalhadji@gmail.com>
2023-03-30 10:35:49 +0700

    Add project files.

---
+++ Act_Quiksort_031.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int arr[20];
+int cmp_count = 0;
+int mov_count = 0;
+int n;
+
+void input() {
+
+}
+int main()
+{
+    std::cout << "Hello World!\n";
+}
