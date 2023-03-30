9b087887ce2aa4eb791ad29226eb7236691e1a5c
Izalhadji31 <ryzalhadji@gmail.com>
2023-03-30 10:59:20 +0700

    menambahkan fungsi swap(int x, int y)

--- Act_Quiksort_031.cpp
+++ Act_Quiksort_031.cpp
@@ -27,6 +27,14 @@ void input() {
     }
 }
 
+void swap(int x, int y)
+{
+    int temp;
+    temp = arr[x];
+    arr[x] = arr[y];
+    arr[y] = temp;
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
